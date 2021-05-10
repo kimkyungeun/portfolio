10b89ab5f0dd1d119732245e6612f6a8afda0275
kimkyungeun <kimkyungeun@kakao.com>
2021-05-10 22:47:56 +0900

    Update comm_frm.js

--- Client/assets/res/www/js/common/comm_frm.js
+++ Client/assets/res/www/js/common/comm_frm.js
@@ -3,15 +3,6 @@
  * <p>comm_frm.js : 공통 프레임</p>
  */
 
-/**
- * 화면 로딩 완료시
- * 화면 로딩완료시 한번 호출된다.
- */
- document.addEventListener("DOMContentLoaded", function(){
- // Handler when the DOM is fully loaded
-
- });
-
 /*********************************************************
  * <b>FUNCTION</b>
  * <p>
