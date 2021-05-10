5f4a66141205852339a45112ff4ceb3f3cfb12cc
kimkyungeun <kimkyungeun@kakao.com>
2021-05-11 01:20:12 +0900

    Create menu.js

---
+++ Client/assets/res/www/js/common/menu.js
@@ -0,0 +1,64 @@
+/**
+ * @file menu.js
+ * <p>menu.js : 메뉴</p>
+ */
+
+ /*********************************************************
+  * <b>FUNCTION</b>
+  * <p>
+  * Function Name : menuHome<br />
+  * Description : 홈 메뉴<br />
+  * </p>
+  *
+  * <p>[수정이력]</p>
+  * <ul>
+  * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+  * </ul>
+  *********************************************************/
+ function menuHome() {
+ }
+
+ /*********************************************************
+  * <b>FUNCTION</b>
+  * <p>
+  * Function Name : menuKKEInfo<br />
+  * Description : 김경은 정보 메뉴<br />
+  * </p>
+  *
+  * <p>[수정이력]</p>
+  * <ul>
+  * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+  * </ul>
+  *********************************************************/
+ function menuKKEInfo() {
+ }
+
+ /*********************************************************
+  * <b>FUNCTION</b>
+  * <p>
+  * Function Name : menuKKEPortfolio<br />
+  * Description : 김경은 포트폴리오 메뉴<br />
+  * </p>
+  *
+  * <p>[수정이력]</p>
+  * <ul>
+  * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+  * </ul>
+  *********************************************************/
+ function menuKKEPortfolio() {
+ }
+
+/*********************************************************
+ * <b>FUNCTION</b>
+ * <p>
+ * Function Name : menuDrawer<br />
+ * Description : 햄버거 메뉴<br />
+ * </p>
+ *
+ * <p>[수정이력]</p>
+ * <ul>
+ * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+ * </ul>
+ *********************************************************/
+function menuDrawer() {
+}
